9cdb49a6147f22b7bc2396f6380a8a151b40ce27
rhel <wer@redhat01.localdomain>
2020-09-06 04:09:52 -0700

    fix

--- inst.sh
+++ inst.sh
@@ -5,6 +5,6 @@ ACOUNT=$(head -n 1 /u01/git/jobs/acount)
 sshpass -p asdf3 ssh puser@10.203.92.232 cat /u01/gnu/eksportal.org/public_page/history.csv > /u01/git/jobs/remote_hist.csv
 
 if [ "$DSOCOUNT" -gt "$ACOUNT" ] || [ "$DSOCOUNT" -lt "$ACOUNT" ]; then
-    echo "$DSOCOUNT" > $ACOUNT
+    echo "$DSOCOUNT" > /u01/git/jobs/acount
     /u01/distr/tlg/telegram -t 1152884180:AAEzbinHoNosvrgNQO7y_A_Fm0Ta_jVo1Vg -c -1001376744472 "started install num $DSOCOUNT"
 fi
