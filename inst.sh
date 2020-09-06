c379f9a32a05af685f226b09671676700b6efeff
coxer20 <417675@mail.ru>
2020-09-06 18:06:43 +0700

    1

--- inst.sh
+++ inst.sh
@@ -4,8 +4,6 @@ ACOUNT=$(head -n 1 /u01/git/jobs/acount)
 
 sshpass -p asdf3 ssh puser@10.203.92.232 cat /u01/gnu/eksportal.org/public_page/history.csv > /u01/git/jobs/remote_hist.csv
 
-echo "$DSOCOUNT" > acount
-
 if [ "$DSOCOUNT" -gt "$ACOUNT" ] || [ "$DSOCOUNT" -lt "$ACOUNT" ]; then
     echo "$DSOCOUNT" > $ACOUNT
     /u01/distr/tlg/telegram -t 1152884180:AAEzbinHoNosvrgNQO7y_A_Fm0Ta_jVo1Vg -c -1001376744472 "started install num $DSOCOUNT"
