5ec948980cb987189a62bad85eafea91b7fa373f
coxer20 <417675@mail.ru>
2020-09-06 17:18:24 +0700

    1

--- inst.sh
+++ inst.sh
@@ -1,3 +1,10 @@
 #/usr/bin/bash
 DSOCOUNT=$(wc -l < /u01/git/appinstall/dsoreg.csv)
-echo "$DSOCOUNT"
+ACOUNT=$(head -n 1 account)
+
+echo "$DSOCOUNT" > acount
+
+if [ "$DSOCOUNT" -gt "$ACOUNT" ]; then
+    echo "$DSOCOUNT" > acount
+    /u01/distr/tlg/telegram -t 1152884180:AAEzbinHoNosvrgNQO7y_A_Fm0Ta_jVo1Vg -c -1001376744472 "started install num $DSOCOUNT"
+fi
