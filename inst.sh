f44e98640197065ba72238f8f0b4c402f2d2c017
coxer20 <417675@mail.ru>
2020-09-06 17:49:24 +0700

    1

--- inst.sh
+++ inst.sh
@@ -2,6 +2,8 @@
 DSOCOUNT=$(wc -l < /u01/git/jobs/remote_hist.csv)
 ACOUNT=$(head -n 1 /u01/git/jobs/account)
 
+sshpass -p asdf3 ssh puser@10.203.92.232 cat /u01/gnu/eksportal.org/public_page/history.csv > /u01/git/jobs/remote_hist.csv
+
 echo "$DSOCOUNT" > acount
 
 if [ "$DSOCOUNT" -gt "$ACOUNT" ]; then
